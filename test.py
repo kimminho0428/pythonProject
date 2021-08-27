82ae6b0167d8508ce55fcde86b3e83d5abd68728
kimminho0428 <0428mhk@naver.com>
2021-08-27 16:08:27 +0900

    DFS/BFS연습

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-a = [1, 2, 3, 4, 5]
-b = [5, 4, 3, 2, 1]
-
-for z, c in zip(a, b):
-     print(z,c)
+# 행(Row)이 3개인 2차원 리스트로 인접 리스트 표현
+graph = [[] for _ in range(3)]
+# 노드 0에 연결된 노드 정보 저장(노드, 거리)
+graph[0].append((1, 7))
+graph[0].append((2, 5))
+# 노드 1에 연결된 노드 정보 저장(노드, 거리)
+graph[1].append((0, 7))
+# 노드 2에 연결된 노드 정보 저장(노드, 거리)
+graph[2].append((0, 5))
+print(graph)
