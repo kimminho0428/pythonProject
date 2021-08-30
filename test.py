06c713a15fdcf349c7d58155f98c7b6d35b3bf7d
kimminho0428 <0428mhk@naver.com>
2021-08-30 16:27:40 +0900

    단어공부

--- test.py
+++ test.py
@@ -1,40 +1,11 @@
-from collections import deque
+word = input().lower()
+word_list = list(set(word)) # 중복제거 후 집합으로 저장
+cnt = [] # 개수를 세는 리스트
+for i in word_list:
+     count = word.count(i) # 단어의 개수를 셈
+     cnt.append(count) # 카운트 한 단어의 개수를 리스트에 저장
 
-# N, M을 공백으로 구분하여 입력받기
-n, m = map(int, input().split())
-# 2차원 리스트의 맵 정보 입력받기
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
-
-# 이동할 네 방향 정의(상, 하, 좌, 우)
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-# BFS 소스코드 구현
-def bfs(x, y):
-    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
-    queue = deque()
-    queue.append((x, y))
-    # 큐가 빌 때까지 반복
-    while queue:
-        x, y = queue.popleft()
-        # 현재 위치에서 네 방향으로의 위치 확인
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            # 미로 찾기 공간을 벗어난 경우 무시
-            if nx < 0 or ny < 0 or nx >= n or ny >= m:
-                continue
-            # 벽인 경우 무시
-            if graph[nx][ny] == 0:
-                continue
-            # 해당 노드를 처음 방문하는 경우에만 최단 거리 기록
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-    # 가장 오른쪽 아래까지의 최단 거리 반환
-    return graph[n - 1][m - 1]
-
-# BFS를 수행한 결과 출력
-print(bfs(0, 0))
+if cnt.count(max(cnt)) >= 2: # 카운트 한 단어의 개수 최댓값이 2개 이상이면 물음표
+    print("?")
+else: # 그렇지 않으면 단어 리스트의 인덱스를 이용하여 단어 대문자 출력
+    print(word_list[(cnt.index(max(cnt)))].upper())
