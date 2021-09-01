274911503b160da75c068b4e11037715461b85f9
kimminho0428 <0428mhk@naver.com>
2021-09-01 17:34:51 +0900

    9월 1일 1

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-word = input().lower()
-word_list = list(set(word)) # 중복제거 후 집합으로 저장
-cnt = [] # 개수를 세는 리스트
-for i in word_list:
-     count = word.count(i) # 단어의 개수를 셈
-     cnt.append(count) # 카운트 한 단어의 개수를 리스트에 저장
-
-if cnt.count(max(cnt)) >= 2: # 카운트 한 단어의 개수 최댓값이 2개 이상이면 물음표
-    print("?")
-else: # 그렇지 않으면 단어 리스트의 인덱스를 이용하여 단어 대문자 출력
-    print(word_list[(cnt.index(max(cnt)))].upper())
+x = int(input())
+line = 1
+while x > line:
+    x -= line # 입력받은 수가 해당 라인의 몇 번째 수인지 나타냄
+    line += 1
+if line % 2 == 0: # line이 짝수일 때
+    a = line - x + 1 # 분자
+    b = x # 분모
+else:
+    a = x
+    b = line - x + 1
+print(a, "/" , b, sep="")
