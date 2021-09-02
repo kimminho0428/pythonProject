d0253c56ef07ac4c0ca4258478a127667649bea0
kimminho0428 <0428mhk@naver.com>
2021-09-02 10:33:49 +0900

    9월 2일 2

--- test.py
+++ test.py
@@ -1,15 +1,14 @@
-n, m = map(int, input().split())
-array = []
-dp = [[0] * (m + 1) for _ in range(n)]
-for _ in range(n):
-    array.append(list(map(int, input().split())))
+m = int(input())
+n = int(input())
+square = []
+i = 1
+while i ** 2 <= n:
+    if m <= i**2 <= n:
+        square.append(i**2)
+    i += 1
 
-for i in range(1, n+1):
-    for j in range(1, m+1):
-        dp[i][j] = array[i][j] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1]
-
-k = int(input())
-for _ in range(k):
-    i, j, x, y = map(int, input().split())
-    print(dp[x][y] - dp[x][j-1] - dp[i-1][y] + dp[x-1][y-1])
-
+if square == []:
+    print(-1)
+else:
+    print(sum(square))
+    print(square[0])
