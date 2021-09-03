151fd5649e90375b3b459776cbb16c1f9b873e2b
kimminho0428 <0428mhk@naver.com>
2021-09-03 09:59:00 +0900

    9월 3일 1

--- test.py
+++ test.py
@@ -1,14 +1,10 @@
-m = int(input())
-n = int(input())
-square = []
-i = 1
-while i ** 2 <= n:
-    if m <= i**2 <= n:
-        square.append(i**2)
-    i += 1
+while True:
+    n = input()
 
-if square == []:
-    print(-1)
-else:
-    print(sum(square))
-    print(square[0])
+    if n == '0':
+        break
+
+    if n == n[::-1]:
+        print('yes')
+    else:
+        print('no')
