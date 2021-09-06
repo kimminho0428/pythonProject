13567e692fa229f284d407b3b6b8164bf8ce4c0a
kimminho0428 <0428mhk@naver.com>
2021-09-06 11:55:43 +0900

    9월 6일 연습3

--- 프로그래머스 LV2/오픈채팅방.py
+++ 프로그래머스 LV2/오픈채팅방.py
@@ -1,18 +1,21 @@
 def solution(record):
     answer = []
     dic = {}
-
+    
     for sentence in record:
+        # 뮨장을 띄어쓰기별로 구분
         sentence_split = sentence.split()
+        # 띄어쓰기가 3개이면 고유아이디(uid)와 닉네임을 딕셔너리에 저장
         if len(sentence_split) == 3:
             dic[sentence_split[1]] = sentence_split[2]
 
     for sentence in record:
         sentence_split = sentence.split()
+        # Enter와 Leave일때 딕셔너리에 있는 닉네임을 불러와 출력
         if sentence_split[0] == 'Enter':
             answer.append('%s님이 들어왔습니다.' % dic[sentence_split[1]])
         elif sentence_split[0] == 'Leave':
             answer.append('%s님이 나갔습니다.' % dic[sentence_split[1]])
 
-    return (answer)
+    return(answer)
 print(solution(["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]))
