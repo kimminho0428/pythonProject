9f3945a917cf9e8ba372efee31fec9a855a7cc70
kimminho0428 <0428mhk@naver.com>
2021-09-06 12:33:15 +0900

    9월 6일 연습5

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-dish = list(input())
-count = 0
-
-for i in range(len(dish)):
-    if i == 0:
-        count = 10
-    elif dish[i] == dish[i-1]:
-        count += 5
-    else:
-        count += 10
-print(count)
+x,y = map(str, input().split())
+s = str(int(x[::-1]) + int(y[::-1]))
+print(int(s[::-1]))
---
+++ 백준 실버문제/셀프 넘버.py
@@ -0,0 +1,11 @@
+natural_num = set(range(1, 10001))
+generated_num = set()
+
+for i in range(1, 10001):
+    for j in str(i):
+        i += int(j)      # 850 + 8 + 5 + 0
+    generated_num.add(i) # 생성자가 있는 숫자들
+
+self_num = sorted(natural_num - generated_num)
+for i in self_num:
+    print(i)
