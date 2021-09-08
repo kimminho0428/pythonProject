edc73f45838df8646fcc7f3d58053dd24b9e358c
kimminho0428 <0428mhk@naver.com>
2021-09-08 11:19:04 +0900

    9월 8일 연습 2

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
-croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
-word = input()
+def solution(msg):
+    answer = []
+    dic = {}
 
-for i in croatia:
-    word = word.replace(i, '*')
-print(len(word))
+    # A부터 Z까지 딕셔너리에 추가
+    for i in range(26):
+        dic[chr(65 + i)] = i + 1
+
+    # 딕셔너리에 없는 단어이면 새롭게 추가, 마지막 단어는 바로 answer에 append하고 종료
+    w, c = 0, 0
+    while True:
+        c += 1
+        if c == len(msg):
+            answer.append(dic[msg[w:c]])
+            break
+
+        if msg[w:c+1] not in dic:
+            dic[msg[w:c+1]] = len(dic) + 1
+            answer.append(dic[msg[w:c]])
+            w = c
+
+
+    return answer
+
+print(solution('ABABABABABABABAB'))
--- 프로그래머스 LV2/압축.py
+++ 프로그래머스 LV2/압축.py
@@ -2,15 +2,18 @@ def solution(msg):
     answer = []
     dic = {}
 
+    # A부터 Z까지 딕셔너리에 추가
     for i in range(26):
         dic[chr(65+i)] = i+1
 
+    # 딕셔너리에 없는 단어이면 새롭게 추가, 마지막 단어는 바로 answer에 append하고 종료
     w, c = 0, 0
     while True:
         c += 1
         if c == len(msg):
             answer.append(dic[msg[w:c]])
             break
+
         if msg[w:c+1] not in dic:
             dic[msg[w:c+1]] = len(dic) + 1
             answer.append(dic[msg[w:c]])
