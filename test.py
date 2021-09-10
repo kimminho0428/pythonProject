1131da9358b932afc433d32ce362bcf944f19de6
kimminho0428 <0428mhk@naver.com>
2021-09-10 11:15:41 +0900

    브루트 포스

--- test.py
+++ test.py
@@ -1,21 +1,31 @@
-def solution(msg):
-    answer = []
-    dic = {}
+import sys
 
-    for i in range(26):
-        dic[chr(65 + i)] = i + 1
+input = sys.stdin.readline()
 
-    w, c = 0, 0
-    while True:
-        c += 1
-        if c == len(msg):
-            answer.append(dic[msg[w:c]])
-            break
-        if msg[w:c+1] not in dic:
-            dic[msg[w:c+1]] = len(dic) + 1
-            answer.append(dic[msg[w:c]])
-            w = c
+n = int(input())
+numbers = list(map(int, input().split()))
+m = int(input)
 
-    return answer
+# dp
+dp = [[0] * n for _ in range(n)]
 
-print(solution('KAKAO'))
+for num_len in range(n):
+    for start in range(n - num_len):
+        end = start + num_len
+
+        # 시작점과 끝점이 같다면 글자수가 1개이므로 무조건 팰린드롬
+        if start == end:
+            dp[start][end] = 1
+        # 시작점의 글자와 끝점의 글자가 같다면
+        elif numbers[start] == numbers[end]:
+            # 두 글자짜리 문자열이라면 무조건 팰린드롬
+            if start + 1 == end:
+                dp[start][end] = 1
+            # 가운데 문자열이 팰린드롬이라면 팰린드롬
+            elif dp[start-1][end-1] == 1:
+                dp[start][end] = 1
+
+# 정답출력하기
+for questions in range(m):
+    s, e = map(int, input().split())
+    print(dp[s-1][e-1])
