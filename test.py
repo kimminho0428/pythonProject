82ccbdea898e21bccf433b8655a6d5a9ebb7e4d2
kimminho0428 <0428mhk@naver.com>
2021-09-10 16:54:16 +0900

    타겟넘버

--- test.py
+++ test.py
@@ -1,13 +1,20 @@
-word = input()
-array = [0] * 10
-for i in range(len(word)):
-    num = int(word[i])
-    if num == 6 or num == 9:
-        if array[6] < array[9]:
-            array[6] += 1
+def solution(numbers, target):
+    answer = 0
+    # 시작점, 인덱스를 큐에 삽입
+    queue = [[numbers[0], 0], [-1 * numbers[0], 0]]
+    n = len(numbers)
+    while queue:
+        tmp, idx = queue.pop()
+        idx += 1
+        if idx < n:
+            # 인덱스 값을 증가시킨 뒤 숫자가 담긴 배열의 크기보다 작으면 노드 +1, 노드 -1을 한 값과 인덱스를 큐에 삽입
+            queue.append([tmp + numbers[idx], idx])
+            queue.append([tmp - numbers[idx], idx])
         else:
-            array[9] += 1
-    else:
-        array[num] += 1
+            # 큐에 들어간 노드가 타겟에 도착하면 answer +1이 증가
+            if tmp == target:
+                answer += 1
+    return answer
 
-print(max(array))
+print(solution([1, 1, 1, 1, 1], 3))
+
--- 프로그래머스 LV2/타겟 넘버.py
+++ 프로그래머스 LV2/타겟 넘버.py
@@ -1,9 +1,11 @@
 # stack을 이용한 dfs여도 마찬가지!
 def solution(numbers, target):
     answer = 0
+    # 시작점, 인덱스를 큐에 삽입
     queue = [[numbers[0], 0], [-1 * numbers[0], 0]]
     n = len(numbers)
     while queue:
+        # 인덱스 값을 증가시킨 뒤 숫자가 담긴 배열의 크기보다 작으면 노드 +1, 노드 -1을 한 값과 인덱스를 큐에 삽입
         temp, idx = queue.pop()
         idx += 1
         if idx < n:
