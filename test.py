a51a0ebfb3440721e5819131b515ee2245a4cc5b
kimminho0428 <0428mhk@naver.com>
2021-09-11 21:20:12 +0900

    9월 11일 커밋

--- test.py
+++ test.py
@@ -1,20 +1,10 @@
-def solution(numbers, target):
-    answer = 0
-    # 시작점, 인덱스를 큐에 삽입
-    queue = [[numbers[0], 0], [-1 * numbers[0], 0]]
-    n = len(numbers)
-    while queue:
-        tmp, idx = queue.pop()
-        idx += 1
-        if idx < n:
-            # 인덱스 값을 증가시킨 뒤 숫자가 담긴 배열의 크기보다 작으면 노드 +1, 노드 -1을 한 값과 인덱스를 큐에 삽입
-            queue.append([tmp + numbers[idx], idx])
-            queue.append([tmp - numbers[idx], idx])
-        else:
-            # 큐에 들어간 노드가 타겟에 도착하면 answer +1이 증가
-            if tmp == target:
-                answer += 1
+def solution(id_list, report, k):
+    answer = []
+    dic = {}
+
+    for id in id_list:
+        dic[]
+
     return answer
 
-print(solution([1, 1, 1, 1, 1], 3))
-
+print(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
