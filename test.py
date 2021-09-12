b62dce5c2a70fce7af839c33d9243437124632c8
kimminho0428 <0428mhk@naver.com>
2021-09-12 11:42:53 +0900

    9월 12일 커밋

--- test.py
+++ test.py
@@ -1,10 +1,28 @@
-def solution(id_list, report, k):
-    answer = []
-    dic = {}
+from collections import Counter
+def solution(str1, str2):
+   str1_low = str1.lower()
+   str2_low = str2.lower()
+   str1_lst = []
+   str2_lst = []
+   for i in range(len(str1_low) - 1):
+       if str1_low[i].isalpha() and str1_low[i+1].isalpha():
+          str1_lst.append(str1_low[i] + str1_low[i+1])
 
-    for id in id_list:
-        dic[]
+   for j in range(len(str2_low) - 1):
+      if str2_low[j].isalpha() and str2_low[j+1].isalpha():
+         str2_lst.append(str2_low[j] + str2_low[j+1])
 
-    return answer
+   Counter1 = Counter(str1_lst)
+   Counter2 = Counter(str2_lst)
 
-print(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
+   inter = list((Counter1 & Counter2).elements())
+   union = list((Counter1 | Counter2).elements())
+
+   if len(inter) == 0 and len(union) == 0:
+      return 65536
+   else:
+      return int(len(inter) / len(union) * 65536)
+
+
+
+print(solution("FRANCE", "french"))
--- 구현/구현_문자열 압축.py
+++ 구현/구현_문자열 압축.py
@@ -1,13 +1,35 @@
 def solution(s):
-    answer = 0
-    count = 1
-    length = int(len(s))
+   answer = len(s)  # 최소 길이
 
-    for i in range(length-1):
-        if s[i] == s[i+1]:
+   # 자르는 단위 늘려가며 최소 길이 계산
+   for slice in range(1, len(s) // 2 + 1):
+      result = ""  # 압축된 문자열
+      count = 1  # 반복되는 횟수
+      compare = s[0:slice]  # 자른 문자열
+
+      # 압축된 문자열 구하기
+      for i in range(slice, len(s), slice):
+         # 문자열이 반복되는 경우
+         if compare == s[i: i + slice]:
             count += 1
-            
+         # 더 이상 압축할 수 없는 경우
+         else:
+            if count == 1:
+               result += compare
+               compare = s[i: i + slice]
+            else:
+               result += (str(count) + compare)
+               count = 1
+               compare = s[i: i + slice]
 
+      # 나머지 문자열 처리
+      if count == 1:
+         result += compare
+      else:
+         result += (str(count) + compare)
 
+      answer = min(answer, len(result))  # 최소 길이 계산
 
-    return answer
+   return answer
+
+print(solution("aabbaccc"))
