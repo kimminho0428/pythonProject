0e4908d861234207d1555d522d63a2f85a5124ab
kimminho0428 <0428mhk@naver.com>
2021-09-16 15:43:01 +0900

    9월 16일 커밋2

--- test.py
+++ test.py
@@ -1,44 +1,14 @@
-n = int(input())
-ans = 1
-candies = []
+n, k = map(int, input().split())
+medals = [list(map(int, input().split())) for _ in range(n)]
+medals.sort(key=lambda x:(x[1], x[2], x[3]), reverse=True)
 
 for i in range(n):
-    temp = []
-    temp_str = input()
-    for j in range(n):
-        temp.append(temp_str[j])
-    candies.append(temp)
-
-def search():
-    global ans
-    for i in range(n):
-        cnt = 1
-        for j in range(n-1):
-            if candies[i][j] == candies[i][j+1]:
-                cnt += 1
-                ans = max(cnt, ans)
-            else:
-                cnt = 1
-
-    for i in range(n):
-        cnt = 1
-        for j in range(n-1):
-            if candies[j][i] == candies[j+1][i]:
-                cnt += 1
-                ans = max(cnt, ans)
-            else:
-                cnt = 1
+    if medals[i][0] == k:
+        idx = i
 
 for i in range(n):
-    for j in range(n-1):
-        candies[i][j], candies[i][j+1] = candies[i][j+1], candies[i][j]
-        search()
-        candies[i][j], candies[i][j + 1] = candies[i][j + 1], candies[i][j]
+    if medals[idx][1:] == medals[i][1:]:
+        print(i+1)
+        break
 
-for i in range(n):
-    for j in range(n-1):
-        candies[j][i], candies[j+1][i] = candies[j+1][i], candies[j][i]
-        search()
-        candies[j][i], candies[j + 1][i] = candies[j + 1][i], candies[j][i]
 
-print(ans)
--- 백준 실버문제/올림픽.py
+++ 백준 실버문제/올림픽.py
@@ -2,7 +2,10 @@ n, k = map(int, input().split())
 medals = [list(map(int, input().split())) for _ in range(n)]
 medals.sort(key = lambda x : (x[1], x[2], x[3]), reverse=True)
 
-idx = [medals[i][0] for i in range(n)].index(k)
+for i in range(n):
+    if medals[i][0] == k:
+        idx = i
+
 for i in range(n):
     if medals[idx][1:] == medals[i][1:]:
         print(i+1)
