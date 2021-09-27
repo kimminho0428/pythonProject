cf043a589138e02c07c66c9d45c200ca12e1041f
kimminho0428 <0428mhk@naver.com>
2021-09-27 11:02:47 +0900

    9월 27일 커밋2

--- test.py
+++ test.py
@@ -1,9 +1,17 @@
-coins = [500, 100, 50, 10]
-n = 1260
-count = 0
-
-for coin in coins:
-    count += n // coin
-    n %= coin
-
-print(count)
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
+first = data[n-1]
+second = data[n-2]
+result = 0
+while True:
+    for i in range(k):
+        if m == 0:
+            break
+        result += first
+        m -= 1
+    if m == 0:
+        break
+    result += second
+    m -= 1
+print(result)
