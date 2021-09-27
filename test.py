656f44d4e96396bdccc245dbe43f8692d0d8edf1
kimminho0428 <0428mhk@naver.com>
2021-09-27 09:32:19 +0900

    9월 27일 커밋

--- test.py
+++ test.py
@@ -1,16 +1,9 @@
-n, m = map(int, input().split())
-data = []
-for _ in range(n):
-    data.append(list(map(int, input())))
-dap = 1
-for i in range(n - 1):
-    for j in range(m - 1):
-        k = 1
-        while 1:
-            if (i + k) == n or (j + k) == m:
-                break
-            if data[i][j] == data[i + k][j] and data[i][j] == data[i][j + k] and data[i][j] == data[i + k][j + k]:
-                dap = max(dap, (k + 1) * (k + 1))
-            k += 1
+coins = [500, 100, 50, 10]
+n = 1260
+count = 0
 
-print(dap)
+for coin in coins:
+    count += n // coin
+    n %= coin
+
+print(count)
