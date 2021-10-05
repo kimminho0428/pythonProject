5189fe3fd9875cbce3bec03da5910e62c2c2c988
kimminho0428 <0428mhk@naver.com>
2021-10-05 12:05:10 +0900

    10월 5일 커밋2

--- test.py
+++ test.py
@@ -1,31 +1,11 @@
-def solution(answers):
-    # 가장 많은 문제를 맞힌 사람을 나타내는 리스트
-    answer = []
-    # 수포자 1
-    answer1 = [1, 2, 3, 4, 5]
-    # 수포자 2
-    answer2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    # 수포자 3
-    answer3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+def solution(brown, yellow):
+    s = brown + yellow
+    for i in range(s, 2, -1):
+        if s % i == 0:
+            a = s // i
+            if yellow == (i-2) * (a-2):
+                return [i, a]
 
-    # 각 수포자가 맞은 수를 기록하는 배열
-    cnt = [0, 0, 0]
-
-    for i in range(len(answers)):
-        if answers[i] == answer1[i % 5]:
-            cnt[0] += 1
-        if answers[i] == answer2[i % 5]:
-            cnt[1] += 1
-        if answers[i] == answer3[i % 10]:
-            cnt[2] += 1
-
-    max_cnt = max(cnt)
-
-    for i in range(3):
-        if max_cnt == cnt[i]:
-            answer.append(i+1)
-
-    return answer
-
-print(solution([1, 2, 3, 4, 5]))
-print(solution([1, 3, 2, 4, 2]))
+print(solution(10, 2))
+print(solution(8, 1))
+print(solution(24, 24))
--- 프로그래머스 LV2/카펫.py
+++ 프로그래머스 LV2/카펫.py
@@ -9,3 +9,19 @@ def solution(brown, yellow):
                     return [a, b]
 
     return answer
+
+print(solution(10, 2))
+
+# 2번째 방법
+def solution(brown, yellow):
+    s = brown + yellow
+    for i in range(s, 2, -1):
+        # 전체 칸에서 i로 나누어지면 i는 가로, s에서 i로 나누면 a는 세로
+        if s % i == 0:
+            a = s // i
+            if yellow == (i-2)*(a-2):
+                return [i, a]
+
+print(solution(10, 2))
+print(solution(8, 1))
+print(solution(24, 24))
