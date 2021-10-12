e4d344f1ee6397397136dfcc51fca47648896db2
kimminho0428 <0428mhk@naver.com>
2021-10-12 15:08:31 +0900

    10월 12일 커밋

--- test.py
+++ test.py
@@ -1,16 +1,20 @@
-def solution(routes):
-    answer = 1
-    routes.sort(key=lambda x: x[0], reverse=True)
-    now = routes[0][0]
+def solution(jobs):
+    answer = 0
+    start = 0
+    length = len(jobs)
+    jobs = sorted(jobs, key=lambda x: x[1])
 
-    for i in routes[1:]:
-        if i[1] >= now:
-            continue
-        else:
-            now = i[0]
-            answer += 1
+    while len(jobs) != 0:
+        for i in range(len(jobs)):
+            if jobs[i][0] <= start:
+                start += jobs[i][1]
+                answer += start - jobs[i][0]
+                jobs.pop(i)
+                break
+            if i == len(jobs) - 1:
+                start += 1
 
-    return answer
+    return answer // length
 
 
-print(solution([[-20, 15], [-14, -5], [-18, -13], [-5, -3]]))
+print(solution([[0, 3], [1, 9], [2, 6]]))
--- 프로그래머스 LV2/디스크 컨트롤러.py
+++ 프로그래머스 LV2/디스크 컨트롤러.py
@@ -3,10 +3,12 @@ def solution(jobs):
     start = 0  # 현재까지 진행된 작업 시간
     length = len(jobs)
 
-    jobs = sorted(jobs, key=lambda x: x[1])  # 소요시간 우선 정렬
+    jobs = sorted(jobs, key=lambda x: x[1])  # 소요시간 우선 정렬, 소요시간이 작을수록 각 작업들이 기다리는 시간이 줄어든다.
 
     while len(jobs) != 0:
         for i in range(len(jobs)):
+            # jobs 길이만큼 for문을 돌리면서 해당 작업의 요청시간이 start(현재까지 진행된 작업 시간)보다 작으면
+            # 즉, 해당 작업이 진행된 시간보다 먼저 요청이 들어왔으면 해당 작업을 진행시키고 jobs 배열에서 지워버린다.
             if jobs[i][0] <= start:
                 start += jobs[i][1]
                 answer += start - jobs[i][0]
