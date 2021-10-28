30628452209be42272a43aafde24dae857f148f3
kimminho0428 <0428mhk@naver.com>
2021-10-28 15:40:02 +0900

    10월 28일 커밋

--- test.py
+++ test.py
@@ -1,22 +1,7 @@
-def solution(phone_number):
+def solution(n, computers):
     answer = 0
 
-    if len(phone_number) == 11 and phone_number not in '-' and phone_number[0] in '0' and phone_number[1] in '1' \
-            and phone_number[2] in '0':
-        answer = 2
-    elif len(phone_number) == 13 and phone_number[3] in '-' and phone_number[8] in '-':
-        answer = 1
-    elif len(phone_number) == 16 and phone_number[0] in '+' and phone_number[1] in '8' and phone_number[2] in '2'\
-            and phone_number[3] in '-' and phone_number[4] in '1' and phone_number[5] in '0' and phone_number[6] in '-'\
-            and phone_number[11] in '-':
-        answer = 3
-    else:
-        answer = -1
 
     return answer
 
-print(solution("01012345678"))
-print(solution("010-1212-3333"))
-print(solution("+82-10-3434-2323"))
-print(solution("+82-010-3434-2323"))
-
+print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]	))
--- 프로그래머스 문제모음/멀쩡한 사각형.py
+++ 프로그래머스 문제모음/멀쩡한 사각형.py
@@ -2,6 +2,7 @@ import math
 
 
 def solution(w, h):
+    # math.gcd는 최대공약수를 구하는 함수
     return w * h - (w + h - math.gcd(w, h))
 
 print(solution(8, 12))
