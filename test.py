a0513848f13c9afa0cc5fd0b74de79eadd174194
kimminho0428 <0428mhk@naver.com>
2021-10-28 19:40:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,26 @@
-def solution(n, computers):
-    answer = 0
+def solution(tickets):
+    answer = []
+
+    routes = dict()
+
+    for t in tickets:
+        if t[0] in routes:
+            routes[t[0]].append(t[1])
+        else:
+            routes[t[0]] = [t[1]]
+
+    for k in routes.keys():
+        routes[k].sort(reverse=True)
+
+    start = ["ICN"]
+    while start:
+        stack = start[-1]
+        if stack not in routes or len(routes[stack]) == 0:
+            answer.append(start.pop())
+        else:
+            start.append(routes[stack].pop())
 
 
-    return answer
+    return answer[::-1]
 
-print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]]	))
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL", "SFO"]]))
