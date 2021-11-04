2722ee05235d5650d99c591d95a1c6db71ef544c
kimminho0428 <0428mhk@naver.com>
2021-11-04 22:54:13 +0900

    22

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
-def solution(n):
-    answer = ''
+def solution(N, number):
+    dp = [[]]
+    for i in range(1, 9):
+        temp = []
+        for j in range(1, i):
+            # j번 사용한 경우의 수 원소 iteration
+            for k in dp[j]:
+                # i-j번 사용한 경우의 수 원소 iteration
+                for l in dp[i - j]:
+                    # 더하기
+                    temp.append(k + l)
+                    if k - l >= 0:
+                        # 빼기
+                        temp.append(k - l)
+                    # 곱하기
+                    temp.append(k * l)
+                    if l != 0 and k != 0:
+                        # 나누기
+                        temp.append(k // l)
 
-    while n > 0:
-        n -= 1
-        answer = '124'[n % 3] + answer
-        n //= 3
+        # 숫자를 그대로 이어 붙인 케이스
+        temp.append(int(str(N) * i))
+        if number in temp:
+            return i
+        dp.append(list(set(temp)))
+    return -1
 
-    return answer
-
-print(solution(1))
-print(solution(2))
-print(solution(3))
-print(solution(4))
+print(solution(5, 12))
--- 프로그래머스 문제모음/N으로 표현.py
+++ 프로그래머스 문제모음/N으로 표현.py
@@ -1,16 +1,25 @@
 def solution(N, number):
+    # 인덱스가 N을 몇 번 사용했는지를 나타냄 ex) dp[1]: 1번 사용, dp[4]: 4번 사용
     dp = [[]]
+    # N 조건이자 사용 횟수 조건(8보다 크면 -1 리턴)
     for i in range(1, 9):
         temp = []
         for j in range(1, i):
+            # j번 사용한 경우의 수 원소 iteration
             for k in dp[j]:
+                # i-j번 사용한 경우의 수 원소 iteration
                 for l in dp[i - j]:
+                    # 더하기
                     temp.append(k + l)
                     if k - l >= 0:
+                        # 빼기
                         temp.append(k - l)
+                    # 곱하기
                     temp.append(k * l)
                     if l != 0 and k != 0:
+                        # 나누기
                         temp.append(k // l)
+        # 숫자를 그대로 이어 붙인 케이스 ex) 55, 555
         temp.append(int(str(N) * i))
         if number in temp:
             return i
