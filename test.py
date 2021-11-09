b640347e53e2ad8db66edf3d44424ffe1c42ec4b
kimminho0428 <0428mhk@naver.com>
2021-11-09 21:45:42 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,17 @@
-def solution(N, number):
-    dp = [[]]
-    for i in range(1, 9):
-        temp = []
-        for j in range(1, i):
-            # j번 사용한 경우의 수 원소 iteration
-            for k in dp[j]:
-                # i-j번 사용한 경우의 수 원소 iteration
-                for l in dp[i - j]:
-                    # 더하기
-                    temp.append(k + l)
-                    if k - l >= 0:
-                        # 빼기
-                        temp.append(k - l)
-                    # 곱하기
-                    temp.append(k * l)
-                    if l != 0 and k != 0:
-                        # 나누기
-                        temp.append(k // l)
+def solution(numbers, target):
+    answer = 0
+    queue = [[numbers[0], 0], [-1 * numbers[0], 0]]
+    n = len(numbers)
+    while queue:
+        tmp, idx = queue.pop()
+        idx += 1
+        if idx < n:
+            queue.append([tmp + numbers[idx], idx])
+            queue.append([tmp - numbers[idx], idx])
+        else:
+            if tmp == target:
+                answer += 1
 
-        # 숫자를 그대로 이어 붙인 케이스
-        temp.append(int(str(N) * i))
-        if number in temp:
-            return i
-        dp.append(list(set(temp)))
-    return -1
+    return answer
 
-print(solution(5, 12))
+print(solution([1, 1, 1, 1, 1], 3))
