b16c60c736d11c74c2c7c992966f7c9998d79d17
kimminho0428 <0428mhk@naver.com>
2021-11-22 16:02:34 +0900

    77

--- test.py
+++ test.py
@@ -1,20 +1,12 @@
-def solution(name):
-    make_name = [min(ord(i) - ord('A'), ord('Z') - ord(i) + 1) for i in name]
-    idx, answer = 0, 0
-    while True:
-        answer += make_name[idx]
-        make_name[idx] = 0
-        if sum(make_name) == 0:
-            break
-        left, right = 1, 1
-        while make_name[idx - left] == 0:
-            left += 1
-        while make_name[idx + right] == 0:
-            right += 1
-        answer += left if left < right else right
-        idx += -left if left < right else right
+def solution(number, k):
+    answer = []
 
-    return answer
+    for num in number:
+        if k > 0 and answer and answer[-1] < num:
+            answer.pop()
+            k -= 1
+        answer.append(num)
 
+    return ''.join(answer[:len(answer) - k])
 
-print(solution("JEROEN"))
+print(solution("1924", 2))
