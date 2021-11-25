f270aaaf8b7e10097333d2f6d293cc2319f68aa7
kimminho0428 <0428mhk@naver.com>
2021-11-25 14:07:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -2,11 +2,12 @@ def solution(number, k):
     answer = []
 
     for num in number:
-        if k > 0 and answer and answer[-1] < num:
+        while k > 0 and answer and answer[-1] < num:
             answer.pop()
             k -= 1
         answer.append(num)
 
     return ''.join(answer[:len(answer) - k])
+
 
 print(solution("1924", 2))
