805e09401dd62376b01d2a7274c2885cfe0aed88
kimminho0428 <0428mhk@naver.com>
2021-12-03 14:07:18 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
-n = int(input())
+input_data = input()
+row = int(input_data[1])
+column = int(ord(input_data[0])) - int(ord('a')) + 1
+steps = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, 1), (2, -1)]
 count = 0
-for i in range(n + 1):
-    for j in range(60):
-        for k in range(60):
-            if '3' in str(i) + str(j) + str(k):
-                count += 1
+
+for step in steps:
+    next_row = row + step[0]
+    next_column = column + step[1]
+
+    if next_row >= 1 and next_row <= 8 and next_column >= 1 and next_column <= 8:
+        count += 1
 
 print(count)
