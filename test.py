7168191211b25572858e3f350842b34922304a15
kimminho0428 <0428mhk@naver.com>
2021-12-03 14:01:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,9 @@
 n = int(input())
-plans = input().split()
-x, y = 1, 1
-move_types = ['L', 'R', 'U', 'D']
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+count = 0
+for i in range(n + 1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
 
-for plan in plans:
-    for i in range(len(move_types)):
-        if plan == move_types[i]:
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-    if nx < 1 or nx > n or ny < 1 or ny > n:
-        continue
-
-    x, y = nx, ny
-
-print(x, y)
+print(count)
