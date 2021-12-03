bb7f7e6e5dcd0b046ff9a57d18f858e65d9c158c
kimminho0428 <0428mhk@naver.com>
2021-12-03 13:59:26 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-def solution(a, b, c, d, e, f):
-    answer = True
+n = int(input())
+plans = input().split()
+x, y = 1, 1
+move_types = ['L', 'R', 'U', 'D']
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
 
-    
+for plan in plans:
+    for i in range(len(move_types)):
+        if plan == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
 
+    if nx < 1 or nx > n or ny < 1 or ny > n:
+        continue
 
-    return answer
+    x, y = nx, ny
 
-print(solution(4, 4, 0, 2, 1, 2))
+print(x, y)
