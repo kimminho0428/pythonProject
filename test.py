d5950bce142570dbd346d06110f14c600c0ec7dd
kimminho0428 <0428mhk@naver.com>
2021-12-07 17:48:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,61 +1,32 @@
-def rotate_a_matrix_by_90_degree(a):
-    # 행 길이 계산
-    n = len(a)
-    # 열 길이 계산
-    m = len(a[0])
-    # 결과 리스트
-    result = [[0] * n for _ in range(m)]
-    for i in range(n):
-        for j in range(m):
-            result[j][n - i - 1] = a[i][j]
-    return result
+n, m = map(int, input().split())
+# 2차원 리스트의 맵 정보 입력받기
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
 
-
-# 자물쇠의 중간 부분이 모두 1인지 확인
-def check(new_lock):
-    lock_length = len(new_lock) // 3
-    for i in range(lock_length, lock_length * 2):
-        for j in range(lock_length, lock_length * 2):
-            if new_lock[i][j] != 1:
-                return False
-
-    return True
-    
-
-
-def solution(key, lock):
-    n = len(lock)
-    m = len(key)
-    # 자물쇠의 크기를 기존의 3배로 변환
-    new_lock = [[0] * (n * 3) for _ in range(n * 3)]
-    # 새로운 자물쇠의 중앙 부분에 기존의 자물쇠 넣기
-    for i in range(n):
-        for j in range(n):
-            new_lock[i + n][j + n] = lock[i][j]
-
-    # 4가지 방향에 대해서 확인
-    for rotation in range(4):
-        # 열쇠 회전
-        key = rotate_a_matrix_by_90_degree(key)
-        for x in range(n * 2):
-            for y in range(n * 2):
-                # 자물쇠에 열쇠 끼어넣기
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] += key[i][j]
-
-                # 새로운 자물쇠에 열쇠가 정확히 들어맞는지 검사
-                if check(new_lock) == True:
-                    return True
-
-                # 자물쇠에서 열쇠를 다시 빼기
-                for i in range(m):
-                    for j in range(m):
-                        new_lock[x + i][y + j] -= key[i][j]
-
-
+# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
+def dfs(x, y):
+    # 주어진 범위를 벗어나는 경우에는 즉시 종료
+    if x <= -1 or x >= n or y <= -1 or y >= m:
+        return False
+    # 현재 노드를 아직 방문하지 않았다면
+    if graph[x][y] == 0:
+        # 해당 노드 방문 처리
+        graph[x][y] = 1
+        # 상, 하, 좌, 우의 위치도 모두 재귀적으로 호출
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
     return False
 
-key = [[0, 0, 0], [1, 0, 0], [0, 1, 1]]
-lock = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
-print(solution(key, lock))
+# 모든 노드(위치)에 대하여 음료수 채우기
+result = 0
+for i in range(n):
+    for j in range(m):
+        # 현재 위치에서 DFS 수행
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
