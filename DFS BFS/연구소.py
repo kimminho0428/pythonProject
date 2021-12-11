b9f39628cc84032bbc4d85104b5320a0a48902c6
kimminho0428 <0428mhk@naver.com>
2021-12-11 17:32:44 +0900

    Update 연구소.py

--- DFS BFS/연구소.py
+++ DFS BFS/연구소.py
@@ -1,67 +1,77 @@
 import sys
+import copy
+
+# sys.setrecursionlimit(10000)
+# sys.stdin = open('./test.txt', 'r')
 input = sys.stdin.readline
-n, m = map(int, input().split())
-data = []  # 초기 맵 리스트
-temp = [[0] * m for _ in range(n)]  # 벽을 설치한 뒤의 맵 리스트
 
-for _ in range(n):
-    data.append(list(map(int, input().split())))
-
-# 4가지 이동 방향에 대한 리스트
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
+n, m = 0, 0
+data = []
 result = 0
+virusList = []
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
 
 
-# 깊이 우선 탐색(DFS)을 이용해 각 바이러스가 사방으로 퍼지도록 하기
-def virus(x, y):
+# 모든 방향으로 바이러스 전파
+def virus(x, y, copyed):
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
         # 상, 하, 좌, 우 중에서 바이러스가 퍼질 수 있는 경우
-        if nx >= 0 and nx < n and ny >= 0 and ny < m:
-            if temp[nx][ny] == 0:
+        if 0 <= nx and nx < n and 0 <= ny and ny < m:
+            if copyed[nx][ny] == 0:
                 # 해당 위치에 바이러스 배치 하고, 다시 재귀적으로 수행
-                temp[nx][ny] = 2
-                virus(nx, ny)
+                copyed[nx][ny] = 2
+                virus(nx, ny, copyed)
 
 
-# 현재 맵에서 안전 영역의 크기 계산하는 메서드
-def get_score():
-    score = 0
+# 안전 영역 크기 계산
+def safe(temp):
+    cnt = 0
     for i in range(n):
         for j in range(m):
             if temp[i][j] == 0:
-                score += 1
-    return score
+                cnt += 1
+    return cnt
 
 
-# 깊이 우선 탐색(DFS)을 이용해 울타리를 설치하면서, 매번 안전 영역의 크기 계산
-def dfs(count):
+def dfs(start, depth):
     global result
     # 울타리가 3개 설치된 경우
-    if count == 3:
-        for i in range(n):
-            for j in range(m):
-                temp[i][j] = data[i][j]
-        # 각 바이러스의 위치에서 전파 진행
-        for i in range(n):
-            for j in range(m):
-                if temp[i][j] == 2:
-                    virus(i, j)
-        # 안전 영역의 최댓값 계산
-        result = max(result, get_score())
+    if depth == 3:
+        # data를 temp에 넣어주고
+        temp = copy.deepcopy(data)
+
+        # 바이러스를 퍼트린 다음
+        for i in range(len(virusList)):
+            [virusX, virusY] = virusList[i]
+            virus(virusX, virusY, temp)
+
+        # 안전한 영역을 계산
+        result = max(result, safe(temp))
         return
-    # 빈 공간에 울타리 설치
+
+    # 2차원 배열에서 숫자를 0부터 n * m 까지 증가시킬 때 (i/m), (i % m)을 좌표로 하면 2차원 배열의 모든 인덱스를 탐색
+    for i in range(start, n * m):
+        x = (int)(i / m)
+        y = (int)(i % m)
+
+        if data[x][y] == 0:
+            data[x][y] = 1
+            dfs(i + 1, depth + 1)
+            data[x][y] = 0
+
+# if __name__=="__main__"이라는 조건문을 넣어주고 그 아래는 직접 실행시켰을 때만 실행되길 원하는 코드들을 넣어주는 것
+if __name__ == '__main__':
+    n, m = map(int, input().split())
+    for i in range(n):
+        data.append(list(map(int, input().split())))
+
     for i in range(n):
         for j in range(m):
-            if data[i][j] == 0:
-                data[i][j] = 1
-                count += 1
-                dfs(count)
-                data[i][j] = 0
-                count -= 1
+            if data[i][j] == 2:
+                virusList.append([i, j])
 
-dfs(0)
+dfs(0, 0)
 print(result)
