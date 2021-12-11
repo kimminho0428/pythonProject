b96b5128354397e81d6660bdfa0cc2c63712e371
kimminho0428 <0428mhk@naver.com>
2021-12-11 16:21:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,32 +1,39 @@
 from collections import deque
 
-n, m = map(int, input().split())
-graph = []
-for i in range(n):
-    graph.append(list(map(int, input())))
+# 도시의 개수, 도로의 개수, 거리 정보, 출발 도시 번호
+n, m, k, x = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
 
-# 상, 하, 좌, 우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+# 모든 도로 정보 입력받기
+for i in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
 
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-    while queue:
-        x, y = queue.popleft()
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+# 모든 도시에 대한 최단거리 초기화
+distance = [-1] * (n + 1)
 
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if graph[nx][ny] == 0:
-                continue
+# 출발 도시까지의 거리는 0으로 설정
+distance[x] = 0
 
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
+# 너비 우선 탐색(BFS) 수행
+q = deque([x])
+while q:
+    now = q.popleft()
+    # 현재 도시에서 이동할 수 있는 모든 도시를 확인
+    for next_node in graph[now]:
+        # 아직 방문하지 않은 도시라면
+        if distance[next_node] == -1:
+            # 최단 거리 갱신
+            distance[next_node] = distance[now] + 1
+            q.append(next_node)
 
-    return graph[n - 1][m - 1]
+# 최단 거리가 K인 모든 도시의 번호를 오름차순으로 출력
+check = False
+for i in range(1, n+1):
+    if distance[i] == k:
+        print(i)
+        check = True
 
-print(bfs(0, 0))
+# 만약 최단거리가 K인 도시가 없다면, -1 출력
+if check == False:
+    print(-1)
