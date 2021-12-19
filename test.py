cd00306dc0f5d5a2f6275403e7056458c44cd4dc
kimminho0428 <0428mhk@naver.com>
2021-12-19 15:24:03 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-# "균형잡힌 괄호 문자열"의 인덱스 반환
 def balanced_index(p):
     count = 0
     for i in range(len(p)):
@@ -9,8 +8,7 @@ def balanced_index(p):
         if count == 0:
             return i
 
-# "올바른 괄호 문자열"인지 판단
-def check_proper(p):
+def checked_proper(p):
     count = 0
     for i in p:
         if i == '(':
@@ -25,29 +23,23 @@ def solution(p):
     answer = ''
     if p == '':
         return answer
-
     index = balanced_index(p)
     u = p[:index+1]
     v = p[index+1:]
-
-    # "올바른 괄호 문자열"이면, v에 대해 함수를 수행한 결과를 붙여 반환
-    if check_proper(u):
+    if checked_proper(u):
         answer = u + solution(v)
-    # "올바른 괄호 문자열"이 아니라면 아래의 과정을 수행
     else:
         answer = '('
         answer += solution(v)
         answer += ')'
-        # 첫 번째와 마지막 문자를 제거
         u = list(u[1:-1])
         for i in range(len(u)):
             if u[i] == '(':
                 u[i] = ')'
             else:
                 u[i] = '('
-        answer += ''.join(u)
+        answer += "".join(u)
+
     return answer
 
-
 print(solution("()))((()"))
-
