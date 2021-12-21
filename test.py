94c264c4cd55ef6ed0ea4a4bb9c121ff99d58efc
kimminho0428 <0428mhk@naver.com>
2021-12-21 21:41:10 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,13 @@
-array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+# 모든 원소의 값이 0보다 크거나 같다고 가정
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+# 모든 범위를 포함하는 리스트 선언(모든 값은 0으로 초기화)
+count = [0] * (max(array) + 1)
+for i in range(len(array)):
+    # 각 데이터에 해당하는 인덱스의 값 증가
+    count[array[i]] += 1
 
-def quick_sort(array):
-    if len(array) <= 1:
-        return array
-
-    pivot = array[0]
-    tail = array[1:]
-
-    left_side = [x for x in tail if x <= pivot]
-    right_side = [x for x in tail if x > pivot]
-
-    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
-
-print(quick_sort(array))
+# 리스트에 기록된 정렬 정보 확인
+for i in range(len(count)):
+    for j in range(count[i]):
+        # 띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
+        print(i, end=' ')
