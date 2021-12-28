a7878cf0ad6e189e1baf4347bbe84ed8109fee6c
kimminho0428 <0428mhk@naver.com>
2021-12-28 22:30:32 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,14 @@
-import heapq
+def sequential_search(n, target, array):
+    for i in range(n):
+        if array[i] == target:
+            return i + 1
 
-n = int(input())
-heap = []
-for i in range(n):
-    data = int(input())
-    heapq.heappush(heap, data)
+print("생성할 원소의 개수를 입력한 다음 한 칸 띄고 찾을 문자열을 입력하세요")
+input_data = input().split()
+n = int(input_data[0])
+target = input_data[1]
 
-result = 0
+print("앞서 적은 원소 개수만큼 문자열을 입력하세요. 구분은 띄어쓰기 한 칸으로 합니다.")
+array = input().split()
 
-while len(heap) != 1:
-    one = heapq.heappop(heap)
-    two = heapq.heappop(heap)
-    sum_value = one + two
-    result += sum_value
-    heapq.heappush(heap, sum_value)
-
-
-print(result)
+print(sequential_search(n, target, array))
