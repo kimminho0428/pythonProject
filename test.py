5b1f9bcdfb9b178977da570791dfc61a4db991f1
kimminho0428 <0428mhk@naver.com>
2021-12-30 17:24:05 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,23 +1,20 @@
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return None
-
-n = int(input())
+n, m = map(int, input().split())
 array = list(map(int, input().split()))
-array.sort()
-m = int(input())
-x = list(map(int, input().split()))
 
-for i in x:
-    result = binary_search(array, i, 0, n-1)
-    if result != None:
-        print('yes', end=' ')
+start = 0
+end = max(array)
+
+result = 0
+while start <= end:
+    total = 0
+    mid = (start + end) // 2
+    for x in array:
+        if x > mid:
+            total += x - mid
+    if total < m:
+        end = mid - 1
     else:
-        print('no', end=' ')
+        result = mid
+        start = mid + 1
+
+print(result)
