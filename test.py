6280ea913f3beee20cf2329236bc7defbf2bfec1
kimminho0428 <0428mhk@naver.com>
2021-12-30 16:13:54 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 def binary_search(array, target, start, end):
-    if start > end:
-        return None
-    mid = (start + end) // 2
-    if array[mid] == target:
-        return mid
-    elif array[mid] > target:
-        return binary_search(array, target, start, mid - 1)
-    else:
-        return binary_search(array, target, mid + 1, end)
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
 
-n, target = list(map(int, input().split()))
+n = int(input())
 array = list(map(int, input().split()))
+array.sort()
+m = int(input())
+x = list(map(int, input().split()))
 
-result = binary_search(array, target, 0, n-1)
-if result == None:
-    print("원소가 존재하지 않습니다.")
-else:
-    print(result + 1)
+for i in x:
+    result = binary_search(array, i, 0, n-1)
+    if result != None:
+        print('yes', end=' ')
+    else:
+        print('no', end=' ')
