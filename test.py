e05718f26d0fc177cf554fd46eb67b9446a158d8
kimminho0428 <0428mhk@naver.com>
2022-01-01 23:15:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,41 @@
 from bisect import bisect_left, bisect_right
 
-# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수
 def count_by_range(a, left_value, right_value):
     right_index = bisect_right(a, right_value)
     left_index = bisect_left(a, left_value)
+
     return right_index - left_index
 
-# 리스트 선언
-a = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9]
+# 모든 단어를 길이마다 나누어서 저장하기 위한 리스트
+array = [[] for _ in range(10001)]
 
-# 값이 4인 데이터의 개수 출력
-print(count_by_range(a, 4, 4))
+# 모든 단어를 길이마다 나누어서 뒤집어 저장하기 위한 리스트
+reversed_array = [[] for _ in range(10001)]
 
-# 값이 [-1, 3] 범위에 있는 데이터 개수 출력
-print(count_by_range(a, -1, 3))
+def solution(words, queries):
+    answer = []
+    # 모든 단어를 접미사 와일드카드 배열, 접두사 와일드카드 배열에 각각 삽입
+    for word in words:
+        # 단어를 삽입
+        array[len(word)].append(word)
+        # 단어를 뒤집어서 삽입
+        reversed_array[len(word)].append(word[::-1])
+
+    # 이진 탐색을 수행하기 위해 각 단어 리스트 정렬 수행
+    for i in range(10001):
+        array[i].sort()
+        reversed_array[i].sort()
+
+    # 쿼리를 하나씩 확인하며 처리
+    for q in queries:
+        # 접미사에 와일드 카드가 붙은 경우
+        if q[0] != '?':
+            res = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
+        # 접두사에 와일드 카드가 붙은 경우
+        else:
+            res = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
+        # 검색된 단어의 개수를 저장
+        answer.append(res)
+    return answer
+
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
