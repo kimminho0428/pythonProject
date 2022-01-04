020d11dfab0a3da96868c0540074f004c82288ce
kimminho0428 <0428mhk@naver.com>
2022-01-04 23:03:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,28 @@
+# 전체 상담 개수
 n = int(input())
-dp = []
+# 각 상담을 완료하는 데 걸리는 기간
+t = []
+# 각 상담을 완료했을 때 받을 수 있는 금액
+p = []
+# 다이나믹 프로그래밍을 위한 1차원 테이블 초기화
+dp = [0] * (n + 1)
+max_value = 0
+
 for _ in range(n):
-    dp.append(list(map(int, input().split())))
+    x, y = map(int, input().split())
+    t.append(x)
+    p.append(y)
 
-for i in range(1, n):
-    for j in range(i + 1):
-        if j == 0:
-            up_left = 0
-        else:
-            up_left = dp[i - 1][j - 1]
-        if j == i:
-            up = 0
-        else:
-            up = dp[i - 1][j]
-        dp[i][j] = dp[i][j] + max(up_left, up)
+# 리스트를 뒤에서부터 거꾸로 확인
+for i in range(n - 1, -1, -1):
+    time = t[i] + i
+    # 상담이 기간 안에 끝나는 경우
+    if time <= n:
+        # 점화식에 맞게, 현재까지의 최고 이익 계산
+        dp[i] = max(p[i] + dp[time], max_value)
+        max_value = dp[i]
+    # 상담이 기간을 벗어나는 경우
+    else:
+        dp[i] = max_value
 
-print(max(dp[n - 1]))
+print(max_value)
