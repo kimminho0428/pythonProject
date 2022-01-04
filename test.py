adbdeb0512ad2352b0a844bc898e43e390403e0b
kimminho0428 <0428mhk@naver.com>
2022-01-04 17:29:00 +0900

    55

--- test.py
+++ test.py
@@ -1,28 +1,18 @@
-for tc in range(int(input())):
-    n, m = map(int, input().split())
-    array = list(map(int, input().split()))
+n = int(input())
+dp = []
+for _ in range(n):
+    dp.append(list(map(int, input().split())))
 
-    dp = []
-    index = 0
-    for i in range(n):
-        dp.append(array[index:index + m])
-        index += m
+for i in range(1, n):
+    for j in range(i + 1):
+        if j == 0:
+            up_left = 0
+        else:
+            up_left = dp[i - 1][j - 1]
+        if j == i:
+            up = 0
+        else:
+            up = dp[i - 1][j]
+        dp[i][j] = dp[i][j] + max(up_left, up)
 
-    for j in range(1, m):
-        for i in range(n):
-            if i == 0:
-                left_up = 0
-            else:
-                left_up = dp[i - 1][j - 1]
-            if i == n - 1:
-                left_down = 0
-            else:
-                left_down = dp[i + 1][j - 1]
-            left = dp[i][j - 1]
-            dp[i][j] = dp[i][j] + max(left_up, left_down, left)
-
-    result = 0
-    for i in range(n):
-        result = max(result, dp[i][m - 1])
-
-    print(result)
+print(max(dp[n - 1]))
--- 다이나믹 프로그래밍/정수 삼각형.py
+++ 다이나믹 프로그래밍/정수 삼각형.py
@@ -1,22 +1,25 @@
 n = int(input())
+# 다이나믹 프로그래밍을 위한 DP 테이블 초기화
 dp = []
 
 for _ in range(n):
     dp.append(list(map(int, input().split())))
 
+# 다이나믹 프로그래밍으로 두 번째 줄부터 내려가면서 확인
 for i in range(1, n):
     for j in range(i+1):
+        # 왼쪽 위에서 내려오는 경우
         if j == 0:
-            left_down = 0
+            up_left = 0
         else:
-            left_down = dp[i-1][j-1]
-
+            up_left = dp[i-1][j-1]
+        # 바로 위에서 내려오는 경우
         if j == i:
-            right_down = 0
+            up = 0
         else:
-            right_down = dp[i-1][j]
-
-        dp[i][j] = dp[i][j] + max(left_down, right_down)
+            up = dp[i-1][j]
+        # 최대 합을 저장
+        dp[i][j] = dp[i][j] + max(up_left, up)
 
 print(max(dp[n-1]))
 
