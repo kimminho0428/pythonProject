b7da7597167a34159b2fd690705de374f02d6f54
kimminho0428 <0428mhk@naver.com>
2022-01-05 15:36:15 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,19 @@
-# 전체 상담 개수
 n = int(input())
-# 각 상담을 완료하는 데 걸리는 기간
-t = []
-# 각 상담을 완료했을 때 받을 수 있는 금액
-p = []
-# 다이나믹 프로그래밍을 위한 1차원 테이블 초기화
-dp = [0] * (n + 1)
-max_value = 0
-for _ in range(n):
-    x, y = map(int, input().split())
-    t.append(x)
-    p.append(y)
+array = list(map(int, input().split()))
 
-# 리스트를 뒤에서부터 거꾸로 확인
-for i in range(n - 1, -1, -1):
-    time = t[i] + i
-    # 상담이 기간 안에 끝나는 경우
-    if time <= n:
-        # 점화식에 맞게, 현재까지의 최고 이익 계산
-        dp[i] = max(p[i] + dp[time], max_value)
-        max_value = dp[i]
-    # 상담이 기간을 벗어나는 경우
-    else:
-        dp[i] = max_value
+# 순서를 뒤집어 '가장 긴 증가하는 부분 수열' 문제로 변환
+array.reverse()
 
-print(max_value)
+# 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
+dp = [1] * n
+
+# 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
+for i in range(1, n):
+    for j in range(0, i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+# 열외시켜야 하는 병사의 최소 수를 출력
+print(n - max(dp))
 
 
