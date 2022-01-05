15cae56aed1b08af27e744f9d2aea4184ca4dedd
kimminho0428 <0428mhk@naver.com>
2022-01-05 16:55:59 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,30 @@
 n = int(input())
-array = list(map(int, input().split()))
 
-# 순서를 뒤집어 '가장 긴 증가하는 부분 수열' 문제로 변환
-array.reverse()
+# 못생긴 수를 담기 위한 테이블(1차원 DP 테이블
+ugly = [0] * n
+# 첫 번째 못생긴 수는 1
+ugly[0] = 1
 
-# 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
-dp = [1] * n
+# 2배, 3배, 5배를 위한 인덱스
+i2, i3, i5 = 0, 0, 0
+# 처음에 곱셈값을 초기화
+next2, next3, next5 = 2, 3, 5
 
-# 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
+# 1부터 n까지의 못생긴 수를 찾기
 for i in range(1, n):
-    for j in range(0, i):
-        if array[j] < array[i]:
-            dp[i] = max(dp[i], dp[j] + 1)
+    # 가능한 곱셈 결과 중에서 가장 작은 수를 선택
+    ugly[i] = min(next2, next3, next5)
+    # 인덱스에 따라서 곱셈 결과를 증가
+    if ugly[i] == next2:
+        i2 += 1
+        next2 = ugly[i2] * 2
+    if ugly[i] == next3:
+        i3 += 1
+        next3 = ugly[i3] * 3
+    if ugly[i] == next5:
+        i5 += 1
+        next5 = ugly[i5] * 5
 
-# 열외시켜야 하는 병사의 최소 수를 출력
-print(n - max(dp))
-
+# n번째 못생긴 수를 출력
+print(ugly[n - 1])
 
