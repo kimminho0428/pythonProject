6dec73036c43713786d5756b73cf9a6b4ee40a50
kimminho0428 <0428mhk@naver.com>
2022-01-05 22:14:54 +0900

    3

--- test.py
+++ test.py
@@ -1,30 +1,63 @@
-n = int(input())
+import sys
+input = sys.stdin.readline
+# 무한을 의미하는 값으로 10억을 설정
+INF = int(1e9)
 
-# 못생긴 수를 담기 위한 테이블(1차원 DP 테이블
-ugly = [0] * n
-# 첫 번째 못생긴 수는 1
-ugly[0] = 1
+# 노드의 개수, 간선의 개수를 입력받기
+n, m = map(int, input().split())
+# 시작 노드 번호를 입력받기
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트를 만들기
+graph = [[] for i in range(n + 1)]
+# 방문한 적이 있는지 체크하는 목적의 리스트를 만들기
+visited = [False] * (n + 1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
 
-# 2배, 3배, 5배를 위한 인덱스
-i2, i3, i5 = 0, 0, 0
-# 처음에 곱셈값을 초기화
-next2, next3, next5 = 2, 3, 5
+# 모든 간선 정보를 입력받기
+for _ in range(m):
+    # a번 노드에서 b번 노드로 가는 비용이 c라는 의미
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
 
-# 1부터 n까지의 못생긴 수를 찾기
-for i in range(1, n):
-    # 가능한 곱셈 결과 중에서 가장 작은 수를 선택
-    ugly[i] = min(next2, next3, next5)
-    # 인덱스에 따라서 곱셈 결과를 증가
-    if ugly[i] == next2:
-        i2 += 1
-        next2 = ugly[i2] * 2
-    if ugly[i] == next3:
-        i3 += 1
-        next3 = ugly[i3] * 3
-    if ugly[i] == next5:
-        i5 += 1
-        next5 = ugly[i5] * 5
+# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
+def get_smallest_node():
+    min_value = INF
+    index = 0
+    # 가장 최단 거리가 짧은 노드(인덱스)
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
 
-# n번째 못생긴 수를 출력
-print(ugly[n - 1])
+    return index
 
+def dijkstra(start):
+    # 시작 노드에 대해서 초기화
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
+    for i in range(n - 1):
+        # 현재 최단 거리가 가장 짧은 노드를 꺼내서, 방문 처리
+        now = get_smallest_node()
+        visited[now] = True
+        # 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+# 다익스트라 알고리즘 수행
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리를 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우, 무한(INFINITY)이라고 출력
+    if distance[i] == INF:
+        print("INFINITY")
+    # 도달할 수 있는 경우 거리를 출력
+    else:
+        print(distance[i])
--- 최단 경로/다익스트라 알고리즘 소스코드.py
+++ 최단 경로/다익스트라 알고리즘 소스코드.py
@@ -22,7 +22,8 @@ for _ in range(m):
 # 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
 def get_smallest_node():
     min_value = INF
-    index = 0 # 가장 최단 거리가 짧은 노드(인덱스)
+    # 가장 최단 거리가 짧은 노드(인덱스)
+    index = 0
     for i in range(1, n+1):
         if distance[i] < min_value and not visited[i]:
             min_value = distance[i]
