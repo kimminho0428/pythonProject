0308809eeddca48adee71cb013e45b269810e78d
kimminho0428 <0428mhk@naver.com>
2022-01-06 16:05:22 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,38 @@
 import heapq
+import sys
 
-def heapsort(iterable):
-    h = []
-    result = []
-    # 모든 원소를 차례대로 힙에 삽입
-    for value in iterable:
-        heapq.heappush(h, value)
+input = sys.stdin.readline
 
-    # 힙에 삽입된 모든 원소를 차례대로 꺼내어 담기
-    for _ in range(len(h)):
-        result.append(heapq.heappop(h))
+INF = int(1e9)
 
-    return result
+n, m = map(int, input().split())
+start = int(input())
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
 
-result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
-print(result)
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra(start)
+
+for i in range(1, n + 1):
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
