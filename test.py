353abacb1ff3fb48e67465e1d20869e3909f12ae
kimminho0428 <0428mhk@naver.com>
2022-01-06 16:57:55 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,25 @@
 INF = int(1e9)
-n = int(input())
-m = int(input())
+n, m = map(int, input().split())
 graph = [[INF] * (n + 1) for _ in range(n + 1)]
-
 for a in range(1, n + 1):
     for b in range(1, n + 1):
         if a == b:
             graph[a][b] = 0
 
 for _ in range(m):
-    a, b, c = map(int, input().split())
-    graph[a][b] = c
+    a, b = map(int, input().split())
+    graph[a][b] = 1
+    graph[b][a] = 1
 
+x, k = map(int, input().split())
 for k in range(1, n + 1):
     for a in range(1, n + 1):
         for b in range(1, n + 1):
             graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
 
-for a in range(1, n + 1):
-    for b in range(1, n + 1):
-        if graph[a][b] == INF:
-            print("INF", end = " ")
-        else:
-            print(graph[a][b], end=" ")
+distance = graph[1][k] + graph[k][x]
 
-    print()
+if distance >= INF:
+    print("-1")
+else:
+    print(distance)
