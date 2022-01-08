932b3e7f0640c0a2c8eff470747b7cff7c11c410
kimminho0428 <0428mhk@naver.com>
2022-01-08 09:57:25 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,30 @@
-def solution(n, lost, reserve):
-    set_reserve = set(reserve) - set(lost)
-    set_lost = set(lost) - set(reserve)
+from bisect import bisect_left, bisect_right
 
-    for i in set_reserve:
-        if i-1 in set_lost:
-            set_lost.remove(i-1)
-        elif i+1 in set_lost:
-            set_lost.remove(i+1)
-    return n - len(set_lost)
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
 
-print(solution(5, [2, 4], [1, 3, 5]))
-print(solution(5, [2, 4], [3]))
-print(solution(3, [3], [1]))
+    return right_index - left_index
+
+array = [[] for _ in range(10001)]
+reversed_array = [[] for _ in range(10001)]
+
+def solution(words, queries):
+    answer = []
+    for word in words:
+        array[len(word)].append(word)
+        reversed_array[len(word)].append(word[::-1])
+
+    for i in range(10001):
+        array[i].sort()
+        reversed_array[i].sort()
+
+    for q in queries:
+        if q[0] != '?':
+            res = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
+        else:
+            res = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
+        answer.append(res)
+    return answer
+
+print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
