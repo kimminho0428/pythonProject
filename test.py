e8ef763eaca2d6fa4ae1461ced65678e50cbe6e5
kimminho0428 <0428mhk@naver.com>
2022-01-08 17:18:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -6,25 +6,11 @@ def count_by_range(a, left_value, right_value):
 
     return right_index - left_index
 
-array = [[] for _ in range(10001)]
-reversed_array = [[] for _ in range(10001)]
+n, x = map(int, input().split())
+array = list(map(int, input().split()))
 
-def solution(words, queries):
-    answer = []
-    for word in words:
-        array[len(word)].append(word)
-        reversed_array[len(word)].append(word[::-1])
-
-    for i in range(10001):
-        array[i].sort()
-        reversed_array[i].sort()
-
-    for q in queries:
-        if q[0] != '?':
-            res = count_by_range(array[len(q)], q.replace('?', 'a'), q.replace('?', 'z'))
-        else:
-            res = count_by_range(reversed_array[len(q)], q[::-1].replace('?', 'a'), q[::-1].replace('?', 'z'))
-        answer.append(res)
-    return answer
-
-print(solution(["frodo", "front", "frost", "frozen", "frame", "kakao"], ["fro??", "????o", "fr???", "fro???", "pro?"]))
+count = count_by_range(array, x, x)
+if count == 0:
+    print(-1)
+else:
+    print(count)
