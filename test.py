ef85e379c910bf0e377e8533e761853b2b6523a8
kimminho0428 <0428mhk@naver.com>
2022-01-12 21:10:19 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,34 +1,35 @@
+import heapq
 import sys
 
 input = sys.stdin.readline
-
 INF = int(1e9)
 
-n, m = map(int, input().split())
-graph = [[INF] * (n + 1) for _ in range(n + 1)]
+# 상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-for a in range(1, n + 1):
-    for b in range(1, n + 1):
-        if a == b:
-            graph[a][b] = 0
+for tc in range(int(input())):
+    n = int(input())
+    graph = []
+    for i in range(n):
+        graph.append(list(map(int, input().split())))
+    distance = [[INF] * n for _ in range(n)]
+    x, y = 0, 0
+    q = [(graph[x][y], x, y)]
+    distance[x][y] = graph[x][y]
+    while q:
+        dist, x, y = heapq.heappop(q)
+        if distance[x][y] < dist:
+            continue
 
-for _ in range(m):
-    a, b = map(int, input().split())
-    graph[a][b] = 1
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if nx < 0  or nx >= n or ny < 0 or ny >= n:
+                continue
+            cost = dist + graph[nx][ny]
+            if cost < distance[nx][ny]:
+                distance[nx][ny] = cost
+                heapq.heappush(q, (cost, nx, ny))
 
-for k in range(1, n + 1):
-    for a in range(1, n + 1):
-        for b in range(1, n + 1):
-            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
-
-result = 0
-for i in range(1, n + 1):
-    count = 0
-    for j in range(1, n + 1):
-        if graph[i][j] != INF or graph[j][i] != INF:
-            count += 1
-
-    if count == n:
-        result += 1
-
-print(result)
+    print(distance[n - 1][n - 1])
