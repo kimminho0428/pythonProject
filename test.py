5ddf831b9a11ee7e491d0ae55476d20febf2105b
kimminho0428 <0428mhk@naver.com>
2022-01-13 18:10:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,35 +1,47 @@
 import heapq
 import sys
 
-input = sys.stdin.readline
 INF = int(1e9)
+input = sys.stdin.readline
 
-# 상, 하, 좌, 우
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+n, m = map(int, input().split())
+graph = [[] for i in range(n + 1)]
+distance = [INF] * (n + 1)
+start = 1
 
-for tc in range(int(input())):
-    n = int(input())
-    graph = []
-    for i in range(n):
-        graph.append(list(map(int, input().split())))
-    distance = [[INF] * n for _ in range(n)]
-    x, y = 0, 0
-    q = [(graph[x][y], x, y)]
-    distance[x][y] = graph[x][y]
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append((b, 1))
+    graph[b].append((a, 1))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
     while q:
-        dist, x, y = heapq.heappop(q)
-        if distance[x][y] < dist:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
             continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if nx < 0  or nx >= n or ny < 0 or ny >= n:
-                continue
-            cost = dist + graph[nx][ny]
-            if cost < distance[nx][ny]:
-                distance[nx][ny] = cost
-                heapq.heappush(q, (cost, nx, ny))
+dijkstra(start)
 
-    print(distance[n - 1][n - 1])
+max_node = 0
+max_distance = 0
+result = []
+
+for i in range(1, n + 1):
+    if max_distance < distance[i]:
+        max_node = i
+        max_distance = distance[i]
+        result = [max_node]
+    elif max_distance == distance[i]:
+        result.append(i)
+
+print(max_node, max_distance, len(result))
+
+
