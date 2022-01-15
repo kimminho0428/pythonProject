59710ae28d9e3db1ab1068a5d448e69715bba51b
kimminho0428 <0428mhk@naver.com>
2022-01-15 16:36:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -13,19 +13,21 @@ def union_parent(parent, a, b):
 
 v, e = map(int, input().split())
 parent = [0] * (v + 1)
+
 for i in range(1, v + 1):
     parent[i] = i
 
+cycle = False
+
 for i in range(e):
     a, b = map(int, input().split())
-    union_parent(parent, a, b)
+    if find_parent(parent, a) == find_parent(parent, b):
+        cycle = True
+        break
+    else:
+        union_parent(parent, a, b)
 
-print('각 원소가 속한 집합: ', end='')
-for i in range(1, v + 1):
-    print(find_parent(parent, i), end=' ')
-
-print()
-
-print('부모 테이블: ', end='')
-for i in range(1, v + 1):
-    print(parent[i], end=' ')
+if cycle:
+    print("사이클이 발생했습니다.")
+else:
+    print("사이클이 발생하지 않았습니다.")
