10926d8944e4e6092590f8db6cbe76127fd8d85a
kimminho0428 <0428mhk@naver.com>
2022-01-15 16:59:17 +0900

    Update test.py

--- test.py
+++ test.py
@@ -17,17 +17,19 @@ parent = [0] * (v + 1)
 for i in range(1, v + 1):
     parent[i] = i
 
-cycle = False
+edges = []
+result = 0
 
-for i in range(e):
-    a, b = map(int, input().split())
-    if find_parent(parent, a) == find_parent(parent, b):
-        cycle = True
-        break
-    else:
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
         union_parent(parent, a, b)
+        result += cost
 
-if cycle:
-    print("사이클이 발생했습니다.")
-else:
-    print("사이클이 발생하지 않았습니다.")
+print(result)
