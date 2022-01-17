45e8db6845724e75201e124d5033850c1211e62d
kimminho0428 <0428mhk@naver.com>
2022-01-17 19:35:49 +0900

    Update test.py

--- test.py
+++ test.py
@@ -13,15 +13,24 @@ def union_parent(parent, a, b):
 
 v, e = map(int, input().split())
 parent = [0] * (v + 1)
-for i in range(v + 1):
+for i in range(1, v + 1):
     parent[i] = i
 
+edges = []
+result = 0
+
 for _ in range(e):
-    oper, a, b = map(int, input().split())
-    if oper == 0:
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+last = 0
+
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
         union_parent(parent, a, b)
-    elif oper == 1:
-        if find_parent(parent, a) == find_parent(parent, b):
-            print('YES')
-        else:
-            print('NO')
+        result += cost
+        last = cost
+
+print(result - last)
