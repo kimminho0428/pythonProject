6a285096a0256b5b85e15b7e828e6d2da38a2676
kimminho0428 <0428mhk@naver.com>
2022-01-17 19:25:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,29 +1,27 @@
-from collections import deque
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
 
 v, e = map(int, input().split())
-indegree = [0] * (v + 1)
-graph = [[] for i in range(v + 1)]
+parent = [0] * (v + 1)
+for i in range(v + 1):
+    parent[i] = i
+
 for _ in range(e):
-    a, b = map(int, input().split())
-    graph[a].append(b)
-    indegree[b] += 1
-
-def topology_sort():
-    result = []
-    q = deque()
-    for i in range(1, v + 1):
-        if indegree[i] == 0:
-            q.append(i)
-
-    while q:
-        now = q.popleft()
-        result.append(now)
-        for i in graph[now]:
-            indegree[i] -= 1
-            if indegree[i] == 0:
-                q.append(i)
-
-    for i in result:
-        print(i, end=' ')
-
-topology_sort()
+    oper, a, b = map(int, input().split())
+    if oper == 0:
+        union_parent(parent, a, b)
+    elif oper == 1:
+        if find_parent(parent, a) == find_parent(parent, b):
+            print('YES')
+        else:
+            print('NO')
