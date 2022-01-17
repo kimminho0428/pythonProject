a91a23bc3a03f6d47be2b417f406aa5996901126
kimminho0428 <0428mhk@naver.com>
2022-01-17 20:06:43 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,36 +1,36 @@
-def find_parent(parent, x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent, parent[x])
-    return parent[x]
+from collections import deque
+import copy
 
-def union_parent(parent, a, b):
-    a = find_parent(parent, a)
-    b = find_parent(parent, b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
+v = int(input())
+graph = [[] for i in range(v + 1)]
+indegree = [0] * (v + 1)
+time = [0] * (v + 1)
 
-v, e = map(int, input().split())
-parent = [0] * (v + 1)
 for i in range(1, v + 1):
-    parent[i] = i
+    data = list(map(int, input().split()))
+    time[i] = data[0]
+    for x in data[1:-1]:
+        indegree[i] += 1
+        graph[x].append(i)
 
-edges = []
-result = 0
+def topology_sort():
+    result = copy.deepcopy(time)
+    q = deque()
+    for i in range(1, v + 1):
+        if indegree[i] == 0:
+            q.append(i)
 
-for _ in range(e):
-    a, b, cost = map(int, input().split())
-    edges.append((cost, a, b))
+    while q:
+        now = q.popleft()
+        for i in graph[now]:
+            result[i] = max(result[i], result[now] + time[i])
+            indegree[i] -= 1
 
-edges.sort()
-last = 0
+            if indegree[i] == 0:
+                q.append(i)
 
-for edge in edges:
-    cost, a, b = edge
-    if find_parent(parent, a) != find_parent(parent, b):
-        union_parent(parent, a, b)
-        result += cost
-        last = cost
+    for i in range(1, v + 1):
+        print(result[i])
 
-print(result - last)
+topology_sort()
+
