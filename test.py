b671e9a163a1b26fc70f7a3ec1c3e44a859ad818
kimminho0428 <0428mhk@naver.com>
2022-01-20 18:04:17 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ def find_parent(parent, x):
         parent[x] = find_parent(parent, parent[x])
     return parent[x]
 
-def union_parent(parent, a , b):
+def union_parent(parent, a, b):
     a = find_parent(parent, a)
     b = find_parent(parent, b)
     if a < b:
@@ -11,24 +11,38 @@ def union_parent(parent, a , b):
     else:
         parent[a] = b
 
-n, m = map(int, input().split())
+n = int(input())
 parent = [0] * (n + 1)
-for i in range(n + 1):
+for i in range(1, n + 1):
     parent[i] = i
 
-edges = []
-total = 0
+x = []
+y = []
+z = []
 result = 0
-for _ in range(m):
-    a, b, cost = map(int, input().split())
-    edges.append((cost, a, b))
+edges = []
+
+for i in range(1, n + 1):
+    data = list(map(int, input().split()))
+    x.append((data[0], i))
+    y.append((data[1], i))
+    z.append((data[2], i))
+
+x.sort()
+y.sort()
+z.sort()
+
+for i in range(n - 1):
+    edges.append((x[i + 1][0] - x[i][0], x[i][1], x[i + 1][1]))
+    edges.append((y[i + 1][0] - y[i][0], y[i][1], y[i + 1][1]))
+    edges.append((z[i + 1][0] - z[i][0], z[i][1], z[i + 1][1]))
 
 edges.sort()
+
 for edge in edges:
     cost, a, b = edge
-    total += cost
     if find_parent(parent, a) != find_parent(parent, b):
         union_parent(parent, a, b)
         result += cost
 
-print(total - result)
+print(result)
