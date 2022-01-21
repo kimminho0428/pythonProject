7b482d21f62273ac50e36263beef47ee9a360af8
kimminho0428 <0428mhk@naver.com>
2022-01-21 17:02:49 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
-n, m = map(int, input().split())
-datas = []
-result = 0
-for i in range(n):
-    datas.append(list(map(int, input().split())))
+n, k = map(int, input().split())
+count = 0
 
-for data in datas:
-    data.sort()
-    result = max(result, data[0])
+while n >= k:
+    while n % k != 0:
+        n -= 1
+        count += 1
+    n //= k
+    count += 1
 
-print(result)
+while n > 1:
+    n -= 1
+    count += 1
+
+print(count)
