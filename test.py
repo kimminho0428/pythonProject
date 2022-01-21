429a048e54d9d8213f93811f7f0d40919fa9653a
kimminho0428 <0428mhk@naver.com>
2022-01-21 16:34:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,19 @@
-n = 1260
-coins = [500, 100, 50, 10]
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
+data.sort()
+first = data[n - 1]
+second = data[n - 2]
 result = 0
-for coin in coins:
-    count = n // coin
-    n %= coin
-    result += count
+while True:
+    for i in range(k):
+        if m == 0:
+            break
+        result += first
+        m -= 1
+
+    if m == 0:
+        break
+    result += second
+    m -= 1
 
 print(result)
