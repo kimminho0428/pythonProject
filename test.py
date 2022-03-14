68315e874d52886ea5abebd66b229ed760f1d552
kimminho0428 <0428mhk@naver.com>
2022-03-14 22:52:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
-n, m = map(int, input().split())
-result = 0
-for i in range(n):
-    data = list(map(int, input().split()))
-    min_value = min(data)
-    result = max(result, min_value)
+import sys
+input = sys.stdin.readline
 
-print(result)
+n, k = map(int, input().split())
+count = 0
+while n >= k:
+    if n % k != 0:
+        n -= 1
+        count += 1
+    n //= k
+    count += 1
+
+while n > 1:
+    n -= 1
+    count += 1
+
+print(count)
