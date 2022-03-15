860dae265c771b7b50893f1da062a2ac6ff6f012
kimminho0428 <0428mhk@naver.com>
2022-03-15 21:56:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,17 @@
 data = input()
-result = int(data[0])
-for i in range(1, len(data)):
-    num = int(data[i])
-    if result <= 1 or num <= 1:
-        result += num
-    else:
-        result *= num
+count0 = 0
+count1 = 0
 
-print(result)
+if data[0] == '1':
+    count0 += 1
+else:
+    count1 += 1
+
+for i in range(len(data) - 1):
+    if data[i] != data[i + 1]:
+        if data[i + 1] == '1':
+            count0 += 1
+        else:
+            count1 += 1
+
+print(min(count0, count1))
