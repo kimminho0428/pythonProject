3c70c5bc1c2a735280d094251b81427cb8f3bc1e
kimminho0428 <0428mhk@naver.com>
2022-03-15 17:24:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
-n = input()
-data = list(map(int, input().split()))
-data.sort()
-
-result = 0
-count = 0
-for i in data:
-    count += 1
-    if count >= i:
-        result += 1
-        count = 0
+data = input()
+result = int(data[0])
+for i in range(1, len(data)):
+    num = int(data[i])
+    if result <= 1 or num <= 1:
+        result += num
+    else:
+        result *= num
 
 print(result)
