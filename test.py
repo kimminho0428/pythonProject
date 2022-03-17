75ad2e66a0088beb03a8fe1a98d46df9258a17c5
kimminho0428 <0428mhk@naver.com>
2022-03-17 21:43:30 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
 s = input()
-result = int(s[0])
-for i in range(1, len(s)):
-    count = int(s[i])
-    if result <= 1 or count <= 1:
-        result += count
-    else:
-        result *= count
+count0 = 0
+count1 = 0
+if s[0] == '0':
+    count1 += 1
+else:
+    count0 += 1
 
-print(result)
+for i in range(len(s) - 1):
+    if s[i] != s[i+1]:
+        if s[i+1] == '1':
+            count0 += 1
+        else:
+            count1 += 1
+
+print(min(count0, count1))
