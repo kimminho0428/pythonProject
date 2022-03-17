52e2b24b11872a1fc3bfb51aa5800e7d1063c52d
kimminho0428 <0428mhk@naver.com>
2022-03-17 22:01:47 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
-s = input()
-count0 = 0
-count1 = 0
-if s[0] == '0':
-    count1 += 1
-else:
-    count0 += 1
+n = int(input())
+data = list(map(int, input().split()))
+data.sort()
+target = 1
 
-for i in range(len(s) - 1):
-    if s[i] != s[i+1]:
-        if s[i+1] == '1':
-            count0 += 1
-        else:
-            count1 += 1
+for x in data:
+    if target < x:
+        break
+    target += x
 
-print(min(count0, count1))
+print(target)
