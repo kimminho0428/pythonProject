bed057083926dd4ccb9d1d878353778ed8116f9c
kimminho0428 <0428mhk@naver.com>
2022-03-23 22:12:10 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import heapq
 
+
 def solution(food_times, k):
     # 전체 음식을 먹는 시간보다 k가 크거나 같다면 -1
     if sum(food_times) <= k:
@@ -27,10 +28,9 @@ def solution(food_times, k):
         # 이전 음식 시간 재설정
         previous = now
     # 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
-    result = sorted(q, key=lambda x:x[1])
+    result = sorted(q, key=lambda x: x[1])
     # 음식의 번호 기준으로 정렬
     return result[(k - sum_value) % length][1]
-
 
 
 print(solution([3, 1, 2], 5))
