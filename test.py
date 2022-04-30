acea46d619065ca55a676d4c5c53d75b1b89c965
kimminho0428 <0428mhk@naver.com>
2022-04-30 17:50:41 +0900

    Update test.py

--- test.py
+++ test.py
@@ -8,14 +8,12 @@ def solution(food_times, k):
     for i in range(len(food_times)):
         # (음식 시간, 음식 번호) 형태로 우선순위 큐에 삽입
         heapq.heappush(q, (food_times[i], i + 1))
-
     # 먹기 위해 사용한 시간
     sum_value = 0
     # 직전에 다 먹은 음식 시간
     previous = 0
     # 남은 음식의 개수
     length = len(food_times)
-
    # sum_value + (현재의 음식 시간 - 이전 음식 시간) * 현재 음식 개수와 k 비교
     while sum_value + ((q[0][0] - previous) * length) <= k:
         now = heapq.heappop(q)[0]
@@ -24,10 +22,8 @@ def solution(food_times, k):
         length -= 1
         # 이전 음식 시간 재설정
         previous = now
-
     # 남은 음식 중에서 몇 번째 음식인지 확인하여 출력
     result = sorted(q, key=lambda x: x[1])
     # 음식의 번호 기준으로 정렬
     return result[(k - sum_value) % length][1]
-
 print(solution([3, 1, 2], 5))
